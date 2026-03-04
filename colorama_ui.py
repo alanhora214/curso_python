2df5ac4906fb183c369286662597bdf39114eb39
Alan Bejarano <alanhoracio2112@gmail.com>
2026-03-03 18:54:25 -0700

    Colorama Tournament

--- colorama_ui.py
+++ colorama_ui.py
@@ -14,7 +14,7 @@ class ColoramaUI:
     def run (self):
         """ Run the colorama ui """
         colorama.init(autoreset=True)
-        self.show_menu()
+        self.display_menu()
     def show_menu(self):
         """ Show the menu """
         while True:
@@ -54,6 +54,46 @@ class ColoramaUI:
         print("Exiting application...")
         exit()
 
+    def get_tournament_json(self):
+        """ Get the tournament """
+        file_path = input("Enter the path to the JSON file: ")
+        self.set_current_file(file_path)
+        self.open_tournament()
+
+    def display_menu(self):
+        """ Display the menu """
+        dictionary_menu = {
+            "1": "Load tournament",
+            "2": "Display tournament",
+            "3": "Exit"
+        }
+        action_dictionary = {
+            "1": self.get_tournament_json,
+            "2": self.display_tournament,
+            "3"
+            "5": self.exit_app
+        }
+
+        while True:
+            print("\nTournament")
+            for key in sorted(dictionary_menu.keys()):
+                print(f"{key}. {dictionary_menu[key]}")
+            choice = input("Enter your choice: ")
+            if choice in action_dictionary:
+                action_dictionary[choice]()
+            else:
+                print("Invalid choice. Please try again.")  
+
+    def display_groups(self):
+        os.system("cls" if os.name == "nt" else "clear")
+        print(Back.LIGHTBLACK_EX + Fore.WHITE + str(self.tournament))
+        for group in self.tournament.groups:
+            print(group)
+        else:
+            print("No tournament loaded.") 
+
 if __name__ == "__main__":
     ui = ColoramaUI()
+    ui.set_current_file("tournament.json")
+    ui.open_tournament
     ui.run()    

625a07a19db3384a85bc80f00e6739f5e8ef1bca
Alan Bejarano <alanhoracio2112@gmail.com>
2026-04-13 18:42:11 -0700

    We add def buscar_palabras

--- books/frases_celebres/frases_celebres.py
+++ books/frases_celebres/frases_celebres.py
@@ -2,7 +2,7 @@
 """
 import csv
 import json
-
+import levenshtein
 
 class Frase:
     def __init__(self, frase, pelicula):
@@ -39,7 +39,37 @@ def carga_archivo_csv(nombre_archivo):
         print(f"Error al cargar el archivo: {e}")
     return frases
 
+def crea_diccionario_titulos(lista_frases)-> dict:
+    diccionario_titulos = {}
+    for frase in lista_frases:
+        if frase.pelicula in diccionario_titulos:
+            diccionario_titulos[frase.pelicula].append(frase)
+        else:
+            diccionario_titulos[frase.pelicula] = [frase]
+        palabras = frase.frase.split()
+        for palabra in palabras:
+            if palabra in diccionario_titulos:
+                diccionario_titulos[palabra].append(frase)
+            else:
+                diccionario_titulos[palabra] = [frase]
+    return diccionario_titulos
+
+def buscar_palabras(frases:list, frase_a_buscar:str)-> list:
+    frases_encontradas = []
+    frase_a_buscar = frase_a_buscar.lower()
+    for frase in frases:
+        frase_lower = frase.frase.lower()
+        ratio = levenshtein.ratio(frase_lower, frase_a_buscar)
+        if ratio >=0.80:
+            frase.ratio = ratio
+            frases_encontradas.append(frase)
+    return frases_encontradas
+
 if __name__ == "__main__":
     frases = carga_archivo_csv("frases_consolidadas.csv")
     for frase in frases[0:5]:
+        print(frase)
+    diccionario_titulos = crea_diccionario_titulos(frases)
+    lista_frase_amor = diccionario_titulos.get("amor")
+    for frase in lista_frase_amor:
         print(frase)
